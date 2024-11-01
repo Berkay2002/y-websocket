2229d2dd9928e2d40bac20d3637e5b305f3f6ae3
Berkay Orhan <beror658@student.liu.se>
2024-11-01 18:55:36 +0100

    Update server.cjs

--- bin/server.cjs
+++ bin/server.cjs
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 const WebSocket = require('ws')
 const http = require('http')
 const number = require('lib0/number')
-const wss = new WebSocket.Server({ noServer: true })
 const setupWSConnection = require('./utils.cjs').setupWSConnection
 
-const host = process.env.HOST || 'localhost'
-const port = number.parseInt(process.env.PORT || '1234')
+// Parsing command-line arguments
+const argv = yargs(hideBin(process.argv)).argv
+
+const host = argv.host || process.env.HOST || '0.0.0.0'
+const port = number.parseInt(argv.port || process.env.PORT || '1234')
+
+const wss = new WebSocket.Server({ noServer: true })
 
 const server = http.createServer((_request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
@@ -17,10 +23,6 @@ const server = http.createServer((_request, response) => {
 wss.on('connection', setupWSConnection)
 
 server.on('upgrade', (request, socket, head) => {
-  // You may check auth of request here..
-  // Call `wss.HandleUpgrade` *after* you checked whether the client has access
-  // (e.g. by checking cookies, or url parameters).
-  // See https://github.com/websockets/ws#client-authentication
   wss.handleUpgrade(request, socket, head, /** @param {any} ws */ ws => {
     wss.emit('connection', ws, request)
   })
